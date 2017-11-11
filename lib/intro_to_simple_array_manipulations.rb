717c81b447a42f457630de6f9fd01accfa69c27c
Leslie Hsu <chuckleberries@hotmail.com>
2017-11-11 19:16:21 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,15 +10,15 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array, number)
-  array.pop(number)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
   array.shift
 end
 
-def shift_with_args(array, number)
+def shift_with_args(array)
   array.shift(2)
 end
 
